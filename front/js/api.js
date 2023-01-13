691c670cc92f0e36bfdd8f7cfaddcd2500ea6e86
Valentin <vf59165@hotmail.com>
2023-01-13 16:43:46 +0100

    feat: add the postOrder api method

--- front/js/api.js
+++ front/js/api.js
@@ -1,9 +1,12 @@
+const HOST = 'localhost';
+const PORT = '3000';
+
 /**
  * Fetch the products from our back-end api
  * @returns [{id: string, colors: [string], name: string, price: number, imageUrl: string, description: string, altTxt: string}]
  */
 export const fetchProducts = async () => {
-  const response = await fetch('http://localhost:3000/api/products');
+  const response = await fetch(`http://${HOST}:${PORT}/api/products`);
   return response.json();
 };
 
@@ -13,6 +16,20 @@ export const fetchProducts = async () => {
  * @returns Promise<array | undefined>
  */
 export const fetchProduct = async (id) => {
-  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+  const response = await fetch(`http://${HOST}:${PORT}/api/products/${id}`);
+  return response.json();
+};
+
+/**
+ * Send a post request to order
+ * @param {{contact: { firstName: string, lastName: string, adress: string, city: string, email: string},
+ *  products: [{id: string, price: string, colors: [string]}]}} order
+ * @returns Promise<object | undefined>
+ */
+export const postOrder = async (order) => {
+  const response = await fetch(`http://${HOST}:${PORT}/api/order/`, {
+    method: 'POST',
+    body: JSON.stringify(order),
+  });
   return response.json();
 };
